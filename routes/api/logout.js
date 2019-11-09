c27f8c8f411def70bc1651dfa8caaef19bf89691
gleaner-mc <765024028@qq.com>
2019-11-09 17:52:41 +0800

    api

---
+++ routes/api/logout.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let router = express.Router();
+let mgdb = require('../../utils/mgdb')
+
+router.delete('/', (req, res, next) => {
+    console.log(req.body)
+    req.session['1909']=undefined;
+    
+  res.send({err:0,msg:'注销成功'})
+})
+
+module.exports = router;
